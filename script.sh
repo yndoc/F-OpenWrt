8fd8907952abdb14803d5f23c08e787102d0d924
ADKill Player Offline <yndoc@users.noreply.github.com>
2023-07-31 16:58:17 +0800

    Update script.sh
    add lucky

--- script.sh
+++ script.sh
@@ -40,3 +40,5 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/l
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 #Add luci-app-adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+#add lucky
+#git clone  https://github.com/gdy666/luci-app-lucky.git package/lean/lucky
