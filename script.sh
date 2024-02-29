3f331b24d54c5d067eb20d7039b1aa339319751c
ADKill Player Offline <yndoc@users.noreply.github.com>
2024-03-01 00:58:28 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -66,5 +66,5 @@ git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mo
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 # iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
+#git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+#git_sparse_clone main https://github.com/linkease/istore luci
